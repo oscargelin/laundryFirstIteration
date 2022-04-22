dd659d4b302c85f63ce773fa7faf15a99a0d290c
Ranjo <oscar.gelin@hotmail.com>
2022-04-22 20:58:24 +0200

    testing Git

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+function App() {
+  return (
+    <div>
+      <h1>Test</h1>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,4 @@
+const Header = () => {
+  return <h2>Header</h2>;
+};
+export default Header;
